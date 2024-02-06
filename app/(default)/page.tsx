98b8ab01a23c2548a537ac85f9beb8ada11528bb
James Brown <jamesraymondbrown@gmail.com>
2024-02-06 12:05:37 -0800

    Modify some boilerplate

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -8,12 +8,10 @@ import Features from "@/components/features";
 import Newsletter from "@/components/newsletter";
 import Zigzag from "@/components/zigzag";
 import Testimonials from "@/components/testimonials";
-import Gallery from "@/components/gallery";
 
 export default function Home() {
   return (
     <>
-      <Gallery />
       <Hero />
       <Features />
       <Zigzag />
--- components/gallery.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useState } from "react";
-
-export default function Gallery() {
-  const [bannerOpen, setBannerOpen] = useState<boolean>(true);
-
-  return <>Gallery</>;
-}
