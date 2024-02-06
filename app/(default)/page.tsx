9f0f91432d95ad88dfe9d73065b37c11611efbb0
James Brown <jamesraymondbrown@gmail.com>
2024-02-06 11:54:37 -0800

    Change styles, add gallery component file

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,22 +1,24 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
-}
+  title: "The Ironwood Co.",
+  description: "Page description",
+};
 
-import Hero from '@/components/hero'
-import Features from '@/components/features'
-import Newsletter from '@/components/newsletter'
-import Zigzag from '@/components/zigzag'
-import Testimonials from '@/components/testimonials'
+import Hero from "@/components/hero";
+import Features from "@/components/features";
+import Newsletter from "@/components/newsletter";
+import Zigzag from "@/components/zigzag";
+import Testimonials from "@/components/testimonials";
+import Gallery from "@/components/gallery";
 
 export default function Home() {
   return (
     <>
+      <Gallery />
       <Hero />
       <Features />
       <Zigzag />
       <Testimonials />
       <Newsletter />
     </>
-  )
+  );
 }
---
+++ components/gallery.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Gallery() {
+  const [bannerOpen, setBannerOpen] = useState<boolean>(true);
+
+  return <>Gallery</>;
+}
